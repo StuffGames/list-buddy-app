743a28a0cce868867d9a27ae5bcc6378bd4ff95e
Sandy Son <sdson@umass.edu>
2024-11-10 06:53:39 -0500

    Tasks load from database now

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -127,6 +127,7 @@ const HomePage = () => {
           throw new Error("No user data found in sessionStorage");
         }
 
+        // IF DOESN'T WORK JUST READ DATA FROM SESSION STORAGE???
         const response = await fetch('/api/tasks/getTasks', {
           method: 'POST',
           headers: {
@@ -141,7 +142,7 @@ const HomePage = () => {
         }
 
         const allTasks = await response.json();
-        setTasks(allTasks.tasks);
+        setTasks(allTasks.totalTasks);
       } catch (err) {
         setError('Failed to get initial data.');
         console.error('Error fetching tasks:', err);
@@ -219,7 +220,7 @@ const HomePage = () => {
                     <span className="ml-2 text-black" >{task.name}</span>
                   </div>
                   <div className="text-sm text-black">
-                    Due {task.deadline.toLocaleDateString()}
+                    Due {new Date(task.deadline).toLocaleDateString()}
                   </div>
                 </li>
               ))}
@@ -248,7 +249,7 @@ const HomePage = () => {
                   <div className="text-sm text-black">
                     Completed {task.completion_date?.toLocaleDateString()}
                   </div>
-                  {task.completion_date <= task.deadline && (
+                  {task.completion_date <= (new Date(task.deadline)) && (
                     <svg width="23" height="22" viewBox="0 0 46 44" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M21.279 1.90679C22.0535 0.59869 23.9465 0.598689 24.721 1.90679L30.7152 12.0311C30.9962 12.5058 31.4607 12.8433 31.9989 12.9638L43.4801 15.5361C44.9635 15.8684 45.5485 17.6688 44.5437 18.8096L36.7672 27.639C36.4026 28.0529 36.2252 28.599 36.2768 29.1481L37.3784 40.8622C37.5207 42.3757 35.9892 43.4884 34.5938 42.8854L23.7934 38.2179C23.2871 37.9991 22.7129 37.9991 22.2066 38.2179L11.4062 42.8853C10.0108 43.4884 8.47931 42.3757 8.62163 40.8622L9.72317 29.1481C9.77481 28.599 9.59737 28.0529 9.23282 27.639L1.4563 18.8096C0.451543 17.6688 1.03652 15.8684 2.51993 15.5361L14.0011 12.9638C14.5393 12.8433 15.0038 12.5058 15.2848 12.0311L21.279 1.90679Z" fill="#FFE100"/>
                     </svg>
@@ -266,7 +267,7 @@ const HomePage = () => {
           .sort((a, b) => a.importance - b.importance) // Sort by importance in ascending order
           .map((task, index) => {
             const daysUntilDeadline = Math.ceil(
-              (task.deadline - new Date()) / (1000 * 60 * 60 * 24)
+              (new Date(task.deadline) - new Date()) / (1000 * 60 * 60 * 24)
             );
             const boxSize = task.priority <= 2 
               ? 'col-span-2 row-span-2' 
